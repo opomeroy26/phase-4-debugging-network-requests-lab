a2b26f9878178a7ade3eccffdf52178acec08a57
Olivia Pomeroy <opomeroy26@gmail.com>
2022-04-07 15:12:41 -0700

    commit tree 684b51e593b7370555804b8964a2aa7eb40998ae

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,16 +7,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :ok
   end
 
   def destroy
+    # byebug
     toy = Toy.find_by(id: params[:id])
     toy.destroy
     head :no_content
